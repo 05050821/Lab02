76fdb79d5efbfcc27fc8bff07ca847231d560a2d
05050821 <37134119+05050821@users.noreply.github.com>
2018-03-14 16:03:52 +0800

    Add files via upload

---
+++ Brave Amur1.ino
@@ -0,0 +1,36 @@
+byte LED[]={4,5,8,9,10,11,12,13},x=0;
+
+void setup()
+{
+	for (byte i = 0; i<= 7; i++) 
+    {
+      pinMode(LED[i], OUTPUT);
+    }
+}
+
+void loop()
+{
+  byte i;
+  for(i=0;i<=7;i++)
+  {
+    digitalWrite(LED[i],LOW);
+  }
+  digitalWrite(LED[x++],HIGH);
+  delay(200);
+  
+  if (x==8) 
+  {
+    x-=1;
+    while(x!=0)
+    {
+    for(i=0;i<=8;i++)
+    {
+      digitalWrite(LED[i],LOW);
+    }
+      digitalWrite(LED[x--],HIGH);
+      delay(200);
+    }
+  }
+}
+
+
